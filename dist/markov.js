10d8f671c4d35cf45bad7bbecf6582684ce32f94
Dane Stuckel <dane.stuckel@gmail.com>
2014-10-01 00:38:08 -0400

    Update version and dist

--- dist/markov.js
+++ dist/markov.js
@@ -1,7 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports.Matrix = require('./lib/matrix');
 module.exports.Chain = require('./lib/chain');
-},{"./lib/chain":2,"./lib/matrix":3}],2:[function(require,module,exports){
+module.exports.Tokenizer = require('./lib/tokenizer');
+},{"./lib/chain":2,"./lib/matrix":3,"./lib/tokenizer":4}],2:[function(require,module,exports){
 var Matrix = require('./matrix');
 
 /**
@@ -261,4 +262,105 @@ var Matrix = (function () {
 })();
 
 module.exports = Matrix;
+},{}],4:[function(require,module,exports){
+var Tokenizer = (function () {
+  var defaults = {
+    separator: / /
+  };
+
+  function readBuffer(options) {
+    var last = '',
+      trailingData = '',
+      regex = options.separator || defaults.separator;
+
+    function addToChart(chart, last, token) {
+      if (!chart[last]) {
+        chart[last] = {};
+      }
+      if (!chart[last][token]) {
+        chart[last][token] = 1;
+      } else {
+        chart[last][token] += 1;
+      }
+    }
+
+    return function(str, chart, isEnd) {
+      chart = chart || {};
+      var match, token, index,
+        lastIndex = 0,
+        lastMatchLength = 0;
+
+      var separator = new RegExp(regex.source, 'g' +
+        (regex.ignoreCase ? 'i' : '') +
+        (regex.multiline ? 'm' : '') +
+        (regex.sticky ? 'y' : ''));
+
+      str = trailingData + str;
+
+      while(match = separator.exec(str)) {
+        index = match.index;
+        token = str.substring(lastIndex + lastMatchLength, index);
+        if (token.length > 0) {
+          if (last.length > 0) {
+            addToChart(chart, last, token);
+          }
+          last = token;
+        }
+        lastIndex = index;
+        lastMatchLength = match[0] && match[0].length || 0;
+      }
+
+      trailingData = str.substr(lastIndex + lastMatchLength);
+      if (!!isEnd && trailingData.length > 0) {
+        addToChart(chart, last, trailingData);
+        trailingData = '';
+      }
+      return chart;
+    }
+  }
+
+  /**
+   * @constructs
+   */
+  var constructor = function (options) {
+    this.options = options || {};
+    this.trailingData = '';
+  };
+  //instance
+  constructor.prototype = {
+    readStream: function (stream, cb) {
+      var chart = {},
+        read = readBuffer(this.options);
+
+      stream.on('error', function (err) {
+        cb(err);
+      });
+      stream.on('data', function (data) {
+        read(data.toString(), chart);
+      });
+      stream.on('end', function () {
+        read('', chart, true);
+        cb(null, chart);
+      });
+
+    },
+    readString: function (str) {
+      var chart = {};
+      readBuffer(this.options)(str, chart, true);
+      return chart;
+    }
+  };
+  //static
+  constructor.Defaults = {
+    'Words': {
+      separator: /\W/
+    },
+    'Whitespace': {
+      separator: /\S/
+    }
+  };
+  return constructor;
+})();
+
+module.exports = Tokenizer;
 },{}]},{},[1]);
